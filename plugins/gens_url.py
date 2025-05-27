18a4d0641131914dc0601cf1158f6c6417b30971
kucingpulung <92753643+kucingpulung@users.noreply.github.com>
2025-05-27 23:18:11 +0700

    Update gens_url.py

--- plugins/gens_url.py
+++ plugins/gens_url.py
@@ -1,7 +1,6 @@
 from hydrogram import Client, filters
 from hydrogram.helpers import ikb
 from hydrogram.types import Message
-from hydrogram.enums import ParseMode
 
 from bot import authorized_users_only, config, helper_handlers, logger, url_safe
 from plugins import list_available_commands
@@ -12,120 +11,38 @@ from plugins import list_available_commands
 )
 @authorized_users_only
 async def generate_handler(client: Client, message: Message) -> None:
+    # Check generate status
     if not helper_handlers.generate_status:
         return
 
     try:
-        # === VARIABEL SETTING MANUAL ===
-        db_caption = "asupan mantap"
-        konten_caption = 'Download media <a href="{link}">disini</a>'
-        konten_chat_id = -1002573263047
+        # Copy the message to the database chat
         database_chat_id = config.DATABASE_CHAT_ID
+        message_db = await message.copy(database_chat_id)
 
-        # === LINK GENERATOR ===
+        # Encode message ID
         encoded_data = url_safe.encode_data(
-            f"id-{abs(database_chat_id) * message.id}"
+            f"id-{message_db.id * abs(database_chat_id)}"
         )
         encoded_data_url = f"https://t.me/{client.me.username}?start={encoded_data}"
+
+        # Create a shareable URL & 
         share_encoded_data_url = f"https://t.me/share/url?url={encoded_data_url}"
-
-        # Format caption
-        final_db_caption = db_caption.replace("{link}", encoded_data_url)
-        final_konten_caption = konten_caption.replace("{link}", encoded_data_url)
-
-        # === KIRIM KE CHANNEL DATABASE ===
-        if message.photo:
-            message_db = await client.send_photo(
-                database_chat_id,
-                photo=message.photo.file_id,
-                caption=final_db_caption,
-                parse_mode=ParseMode.HTML
-            )
-        elif message.document:
-            message_db = await client.send_document(
-                database_chat_id,
-                document=message.document.file_id,
-                caption=final_db_caption,
-                parse_mode=ParseMode.HTML
-            )
-        elif message.video:
-            message_db = await client.send_video(
-                database_chat_id,
-                video=message.video.file_id,
-                caption=final_db_caption,
-                parse_mode=ParseMode.HTML
-            )
-        elif message.audio:
-            message_db = await client.send_audio(
-                database_chat_id,
-                audio=message.audio.file_id,
-                caption=final_db_caption,
-                parse_mode=ParseMode.HTML
-            )
-        elif message.text:
-            message_db = await client.send_message(
-                database_chat_id,
-                text=final_db_caption,
-                parse_mode=ParseMode.HTML
-            )
-        else:
-            message_db = await message.copy(database_chat_id)
-
-        # === KIRIM KE CHANNEL KONTEN ===
-        if message.photo:
-            konten_msg = await client.send_photo(
-                konten_chat_id,
-                photo=message.photo.file_id,
-                caption=final_konten_caption,
-                parse_mode=ParseMode.HTML
-            )
-        elif message.document:
-            konten_msg = await client.send_document(
-                konten_chat_id,
-                document=message.document.file_id,
-                caption=final_konten_caption,
-                parse_mode=ParseMode.HTML
-            )
-        elif message.video:
-            konten_msg = await client.send_video(
-                konten_chat_id,
-                video=message.video.file_id,
-                caption=final_konten_caption,
-                parse_mode=ParseMode.HTML
-            )
-        elif message.audio:
-            konten_msg = await client.send_audio(
-                konten_chat_id,
-                audio=message.audio.file_id,
-                caption=final_konten_caption,
-                parse_mode=ParseMode.HTML
-            )
-        elif message.text:
-            konten_msg = await client.send_message(
-                konten_chat_id,
-                text=final_konten_caption,
-                parse_mode=ParseMode.HTML
-            )
-        else:
-            konten_msg = await message.copy(konten_chat_id)
-
-        # === BUAT LINK UNTUK PESAN ===
-        konten_url = f"https://t.me/c/{str(konten_chat_id)[4:]}/{konten_msg.id}"
         db_url = f"https://t.me/c/{str(database_chat_id)[4:]}/{message_db.id}"
-
-        # === BALAS KE USER DENGAN KETERANGAN SENDIRI ===
+        
+        # Reply to the user with the generated URL
         await message.reply_text(
-            f'Here is your link:\n<code>{encoded_data_url}</code>',
+            encoded_data_url,
             quote=True,
-            parse_mode=ParseMode.HTML,
-            reply_markup=ikb([
-                [("Share", share_encoded_data_url, "url")],
-                [("Lihat Konten", konten_url, "url")],
-                [("Lihat DB", db_url, "url")]
-            ]),
+            reply_markup=ikb([[("Share", share_encoded_data_url, "url")]]),
             disable_web_page_preview=True,
+            reply_markup=ikb([
+                [("Share", share_encoded_data_url, "url")], 
+                [("Lihat DataBase", db_url, "url")]
+            ]),
+    
         )
-
     except Exception as exc:
+        # Log the error and inform the user
         logger.error(f"Generator: {exc}")
         await message.reply_text("<b>An Error Occurred!</b>", quote=True)
